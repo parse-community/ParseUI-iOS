01b76cefcf52a4713e97b5a880ef37ea9b91485f
Eugenio Baglieri <baglieri.eugenio@gmail.com>
2015-11-26 22:14:22 +0100

    PFTableFooterLoadingView view created

---
+++ ParseUI/Classes/Internal/Views/PFTableFooterLoadingView.h
@@ -0,0 +1,16 @@
+//
+//  PFTableFooterLoadingView.h
+//  Pods
+//
+//  Created by Eugenio Baglieri on 26/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+/*!
+ A loading view that is used as tableview footer to show users that data is being loaded while scrolling
+ */
+@interface PFTableFooterLoadingView : UIView
+
+@end
