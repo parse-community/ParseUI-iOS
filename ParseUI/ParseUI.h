2df94e715730844c366ed64f663df0ae473895db
Kila2 <277014717@qq.com>
2017-03-14 14:32:28 +0800

    add framework

---
+++ ParseUI/ParseUI.h
@@ -0,0 +1,19 @@
+//
+//  ParseUI.h
+//  ParseUI
+//
+//  Created by junlianglee on 2017/3/14.
+//  Copyright © 2017年 Parse Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParseUI.
+FOUNDATION_EXPORT double ParseUIVersionNumber;
+
+//! Project version string for ParseUI.
+FOUNDATION_EXPORT const unsigned char ParseUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParseUI/PublicHeader.h>
+
+
